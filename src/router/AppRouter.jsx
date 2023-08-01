781a4dc71e7c6963ea9cc7a374171415f549697b
Beto <ivjalberto@gmail.com>
2023-08-01 15:20:47 -0600

    Clase 191

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,15 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 
-import { LoginPage } from '../auth/pages/LoginPage'
-import { DcPage } from '../heroes/pages/DcPage'
-import { MarvelPage } from '../heroes/pages/MarvelPage'
+import { LoginPage } from '../auth/pages'
+import { MarvelPage, DcPage } from '../heroes/pages'
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
     <>
+
+        <Navbar/>
+
         <Routes>
             <Route path="marvel" element={<MarvelPage/>}/>
             <Route path="dc" element={<DcPage/>}/>
