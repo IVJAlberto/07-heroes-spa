d3587c498ebcc4f3b40d69cc8608894281bbf345
Beto <ivjalberto@gmail.com>
2023-08-01 16:36:05 -0600

    Clase 194

---
+++ src/heroes/components/HeroeList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { getHeroesByPublisher } from '../helpers'
+
+export const HeroeList = ( { publisher } ) => {
+
+    const heroes = getHeroesByPublisher( publisher );
+
+    return (
+        <ul className='list-group'>
+            {
+                heroes.map( heroe => (
+                    <li key={ heroe.id}>
+                        {
+                            heroe.superhero
+                        }
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -1,8 +1,13 @@
+import { HeroeList } from "../components/HeroeList"
 
 export const MarvelPage = () => {
     return (
       <>
           <h1>Marvel Page</h1>
+          <hr />
+
+          <HeroeList publisher={'Marvel Comics'} />
+
       </>
     )
   }
