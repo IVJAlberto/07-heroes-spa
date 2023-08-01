8c5021c70a7c1e3c9d23c1ae5b3f1a9b8772858b
Beto <ivjalberto@gmail.com>
2023-08-01 17:20:12 -0600

    Clase 197

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,20 @@
+import { Navigate, useParams } from "react-router-dom"
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+
+  const { id } = useParams();
+
+  const hero = getHeroById(id);
+
+  if( !hero )
+    return <Navigate to="/marvel"/>
+
   return (
-    <div>HeroPage</div>
+    <>
+      <h1>{ hero.superhero }</h1>
+      <hr />
+
+    </>
   )
 }
