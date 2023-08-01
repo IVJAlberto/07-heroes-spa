1bd1f012aabe9eac14abdd8847997fe57aa12520
Beto <ivjalberto@gmail.com>
2023-08-01 15:51:25 -0600

    Clase 192

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,22 +1,15 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { LoginPage } from '../auth/pages'
-import { MarvelPage, DcPage } from '../heroes/pages'
-import { Navbar } from "../ui";
+import { HeroesRoutes } from '../heroes';
 
 export const AppRouter = () => {
   return (
     <>
-
-        <Navbar/>
-
         <Routes>
-            <Route path="marvel" element={<MarvelPage/>}/>
-            <Route path="dc" element={<DcPage/>}/>
-
             <Route path="login" element={<LoginPage/>}/>
+            <Route path="/*" element={<HeroesRoutes/>}/>
 
-            <Route path="/" element={<Navigate to="/marvel"/>}/>
         </Routes>
     </>
   )
