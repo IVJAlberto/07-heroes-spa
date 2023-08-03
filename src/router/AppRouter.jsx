4d561b36c96bbb4bb2b3a86765972f1aefd10788
Beto <ivjalberto@gmail.com>
2023-08-03 14:07:51 -0600

    Clase 214

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,19 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { LoginPage } from '../auth/pages'
 import { HeroesRoutes } from '../heroes';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
   return (
     <>
         <Routes>
             <Route path="login" element={<LoginPage/>}/>
-            <Route path="/*" element={<HeroesRoutes/>}/>
+
+            <Route path='/*' element={
+              <PrivateRoute>
+                <HeroesRoutes/>
+              </PrivateRoute>
+            }/>
 
         </Routes>
     </>
